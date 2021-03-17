fc58407ba719b9178582c2cb7aa07d3c404c7d99
yohann paulin <yohannpaulin@hotmail.fr>
2021-03-17 15:45:13 +0100

    implementation des controller restaurant et review

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants do
+  resources :restaurants, only: [:index, :new, :show, :create] do
     resources :reviews, only: [:new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-restaurants = Restaurant.create([{name: "baybua" }, {address: "Chemin des Croix-Rouges 8, 1007 Lausanne" }, {phone_number:" 021 219 27 97" }, {category:"chinese" }])
-restaurants = Restaurant.create([{name: "Golden Mango" }, {address: " Avenue de Morges 92, 1004 Lausanne" }, {phone_number: "079 375 99 88"}, {category: "french"}])
-restaurants = Restaurant.create([{name: "Sakura"}, {address: "Chemin de Pierrefleur 30, 1004 Lausanne"}, {phone_number: "021 646 99 66"}, {category: "japanese"}])
-restaurants = Restaurant.create([{name: "Restaurant italien Ulivo" }, {address: "Chemin de Couvaloup 13, 1005 Lausanne"}, {phone_number: "021 544 66 82" }, {category: "italian"}])
-restaurants = Restaurant.create([{name: "Bruxelles Café"}, {address:"Place de la Riponne 1, 1005 Lausanne" }, {phone_number: "021 311 33 01"}, {category: "belgian"}])
+restaurants = Restaurant.create([{ name: 'baybua' }, { address: 'Chemin des Croix-Rouges 8, 1007 Lausanne' }, { phone_number: ' 021 219 27 97' }, { category: 'chinese' }])
+restaurants = Restaurant.create([{ name: 'Golden Mango' }, { address: ' Avenue de Morges 92, 1004 Lausanne' }, { phone_number: '079 375 99 88' }, { category: 'french' }])
+restaurants = Restaurant.create([{ name: 'Sakura' }, { address: 'Chemin de Pierrefleur 30, 1004 Lausanne' }, { phone_number: '021 646 99 66' }, { category: 'japanese' }])
+restaurants = Restaurant.create([{ name: 'Restaurant italien Ulivo' }, { address: 'Chemin de Couvaloup 13, 1005 Lausanne' }, { phone_number: '021 544 66 82' }, { category: 'italian' }])
+restaurants = Restaurant.create([{ name: 'Bruxelles Café' }, { address: 'Place de la Riponne 1, 1005 Lausanne' }, { phone_number: '021 311 33 01' }, { category: 'belgian' }])
