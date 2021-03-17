27c17dc5e570c0f2e80312c67af9175c4445a2c6
yohann paulin <yohannpaulin@hotmail.fr>
2021-03-17 14:13:32 +0100

    validation of models added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,24 @@
 class RestaurantsController < ApplicationController
+  def index
+  end
+  def show
+    @restaurant = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new(attribute)
+
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
